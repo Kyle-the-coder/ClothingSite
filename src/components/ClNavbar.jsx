27a0e7f9d7e9f8d75481ae89107dda0b499328b5
Kyle Mitchell <kylethacoder@gmail.com>
2023-05-13 12:54:55 -0700

    updated

--- src/components/ClNavbar.jsx
+++ src/components/ClNavbar.jsx
@@ -1,11 +1,26 @@
+import logo from "../assets/imgs/rblogo.jpeg"
 
+let navLinks = [
+    { title: "Home", link: "link" },
+    { title: "Merch", link: "link" },
+    { title: "Contact", link: "link" },
+]
 
 const ClNavbar = () => {
     return (
-        <div>
-            <h1 className="text-3xl font-bold text-red-200 underline">
-                Rosa Buck
-            </h1>
+        <div className="w-full h-[200px] bg-black flex justify-center items-center justify-between">
+            <div className="w-[200px] h-[200px] flex ml-[30px]">
+                <img src={logo} className="rounded cursor-pointer" />
+            </div>
+            <div className="flex w-[300px] justify-between mr-[50px]">
+                {navLinks.map((link, i) => {
+                    return (
+                        <div key={i}>
+                            <h1 className="text-green-500 cursor-pointer">{link.title}</h1>
+                        </div>
+                    )
+                })}
+            </div>
         </div>
     )
 }
