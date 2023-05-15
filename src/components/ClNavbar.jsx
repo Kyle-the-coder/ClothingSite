adcc49a3b5830a07ebba091713b68aa819e40031
Kyle Mitchell <kylethacoder@gmail.com>
2023-05-15 15:11:27 -0700

    updated

--- src/components/ClNavbar.jsx
+++ src/components/ClNavbar.jsx
@@ -8,14 +8,14 @@ let navLinks = [
 
 const ClNavbar = () => {
     return (
-        <div className="w-full h-[200px] bg-black flex justify-center items-center justify-between">
-            <div className="w-[200px] h-[200px] flex ml-[30px]">
+        <div className="w-full h-[200px] bg-black flex justify-center items-center justify-between z-[11] absolute">
+            <div className="w-[200px] h-[200px] flex ml-[30px] z-[10]">
                 <img src={logo} className="rounded cursor-pointer" />
             </div>
-            <div className="flex w-[300px] justify-between mr-[50px]">
+            <div className="flex w-[300px] justify-between mr-[50px] z-[10]">
                 {navLinks.map((link, i) => {
                     return (
-                        <div key={i}>
+                        <div className="z-[10]" key={i}>
                             <h1 className="text-green-600 cursor-pointer">{link.title}</h1>
                         </div>
                     )
--- src/views/LandingPage.jsx
+++ src/views/LandingPage.jsx
@@ -1,11 +1,41 @@
-import rbphoto from "../assets/imgs/rbjdemofront.jpeg"
+import rbphoto from "../assets/imgs/Subject.png"
 
 const LandingPage = () => {
-    return(
-        <div>
-            Hello from landing page!
+    return (
+        <div className="overflow-hidden z-[999]">
+            <div className="bg-green-600 w-screen h-[1160px] border-black border-b-[4px] flex justify-end relative">
+                <div className="bg-white w-screen h-full rounded-bl-[1250px] rounded-b-[10rem] absolute right-[100px] flex relative">
+                    <div className="absolute top-[200px] left-[200px] z-[10] relative">
+
+                        <img src={rbphoto} />
+                        <div className="absolute z-[11] top w-full h-content p-3 bg-slate-200 top-[50px] rounded-[3.4rem] left-[580px] border-2 border-red-600">
+                            <h1 className="underline py-2 text-lg">Rosa Buck History</h1>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. At urna condimentum mattis pellentesque id nibh tortor id. Sagittis orci a scelerisque purus semper. Lacus vestibulum sed arcu non odio euismod. Sed risus ultricies tristique nulla aliquet. Sapien eget mi proin sed. Leo integer malesuada nunc vel risus. Quis vel eros donec ac odio tempor. Malesuada fames ac turpis egestas sed tempus urna et pharetra. Sit amet tellus cras adipiscing enim eu turpis egestas pretium.
+                                Elementum pulvinar etiam non quam lacus. Magnis dis parturient montes nascetur ridiculus mus mauris vitae.
+                                Lacinia at quis risus sed vulputate odio ut.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+
             <div>
-                <img src={rbphoto}/>
+                <h1>Rosa Buck circa 2017 </h1>
+                <h1>Rosa Buck circa 2017 </h1>
+                <h1>Rosa Buck circa 2017 </h1>
+                <h1>Rosa Buck circa 2017 </h1>
+                <h1>Rosa Buck circa 2017 </h1>
+                <h1>Rosa Buck circa 2017 </h1>
+                <h1>Rosa Buck circa 2017 </h1>
+                <h1>Rosa Buck circa 2017 </h1>
+                <h1>Rosa Buck circa 2017 </h1>
+                <h1>Rosa Buck circa 2017 </h1>
+                <h1>Rosa Buck circa 2017 </h1>
+                <h1>Rosa Buck circa 2017 </h1>
+                <h1>Rosa Buck circa 2017 </h1>
             </div>
         </div>
     )
