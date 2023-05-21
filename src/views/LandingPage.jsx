932a83dfa191703d93ee3edb04d6257b44831515
Kyle Mitchell <kylethacoder@gmail.com>
2023-05-21 11:33:41 -0700

    updated

--- src/views/LandingPage.jsx
+++ src/views/LandingPage.jsx
@@ -16,6 +16,18 @@ const LandingPage = () => {
             <div className="w-full bg-slate-900 h-300px">
                 <MyIcon/>
             </div>
+            <div>
+                <h1>Keep Going</h1>
+            </div>
+            <div>
+                <h1>Keep Going</h1>
+            </div>
+            <div>
+                <h1>Keep Going</h1>
+            </div>
+            <div>
+                <h1>Keep Going</h1>
+            </div>
         </div>
     )
 }
